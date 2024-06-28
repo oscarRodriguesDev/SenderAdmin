eec5ded9589f54de64c4dbcbe8e36b04eba31921
Oscar <oskharm12@gmail.com>
2024-06-27 22:15:34 -0300

    visualizaçção para todos os usuarios atulaizada

--- src/components/dashComponents/Tables/allUsers.tsx
+++ src/components/dashComponents/Tables/allUsers.tsx
@@ -150,10 +150,8 @@ const TableOne = () => {
     <div className="rounded-[10px] bg-white px-2 pb-2 pt-2 shadow-1 dark:bg-gray-dark dark:shadow-card">
       <Toaster/>
       <div className="bg-slate-50 flex flex-col rounded-lg">
-        <div className="grid grid-cols-8 gap-1">
-          <div className="px-2 pb-3.5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base text-black">CPF</h5>
-          </div>
+        <div className="grid grid-cols-4 gap-1">
+        
           <div className="px-2 pb-3.5 text-center">
             <h5 className="text-sm font-medium uppercase xsm:text-base text-black">Nome</h5>
           </div>
@@ -164,15 +162,6 @@ const TableOne = () => {
             <h5 className="text-sm font-medium uppercase xsm:text-base text-black">Contrato</h5>
           </div>
           <div className="px-2 pb-3.5 text-center sm:block">
-            <h5 className="text-sm font-medium uppercase xsm:text-base text-black">DOC</h5>
-          </div>
-          <div className="px-2 pb-3.5 text-center sm:block">
-            <h5 className="text-sm font-medium uppercase xsm:text-base text-black">Data</h5>
-          </div>
-          <div className="px-2 pb-3.5 text-center sm:block">
-            <h5 className="text-sm font-medium uppercase xsm:text-base text-black">Status</h5>
-          </div>
-          <div className="px-2 pb-3.5 text-center sm:block">
             <h5 className="text-sm font-medium uppercase xsm:text-base text-black">DEL</h5>
           </div>
           
@@ -181,15 +170,11 @@ const TableOne = () => {
 
         {data.map((item, key) => (
           <div
-            className={`grid grid-cols-8 gap-1 ${key === data.length - 1 ? "" : "border-b border-stroke dark:border-dark-3"
+            className={`grid grid-cols-4 gap-1 ${key === data.length - 1 ? "" : "border-b border-stroke dark:border-dark-3"
               }`}
             key={key}
           >
-            <div className="flex items-center gap-3.5 px-2 py-4">
-              <div className="flex items-center justify-center px-2 py-4">
-                <p className="font-medium dark:text-slate-900">{item.CPF}</p>
-              </div>
-            </div>
+           
             <div className="flex items-center justify-center px-2 py-4">
               <p className="font-medium text-dark">{item.nome}</p>
             </div>
@@ -198,37 +183,6 @@ const TableOne = () => {
             </div>
             <div className="flex items-center justify-center px-2 py-4">
               <p className="font-medium text-dark">{item.contrato}</p>
-            </div>
-            <div className="flex items-center justify-center px-2 py-4 sm:flex">
-              <p className="font-medium text-dark">
-                <a href={item.url} target="_blank"><FcViewDetails size={24} /></a>
-              </p>
-            </div>
-            <div className="flex items-center justify-center px-2 py-4 sm:flex">
-              <p className="font-medium text-dark">{item.ultima_data}</p>
-            </div>
-          
-          
-            <div className="flex items-center justify-center px-2 py-4 sm:flex">
-              <p className="font-medium text-dark flex gap-3">
-                {/* Botão "Reprovado" */}
-                <button
-                title='Click para reprovar o atestado'
-                  id='disapprov'
-                  className={`border border-solid border-blue-400 rounded-full p-2 hover:bg-slate-400 ${(item.aprove=='aprovado') ? 'bg-white' : 'bg-amber-200'}`}
-                  onClick={() => handleButtonClick(item.CPF, 'reprovado')}>
-                  <TbFileDislike size={18} color={'#f93c3c'} />
-                </button>
-
-                {/* Botão "Aprovado" */}
-                <button
-                 title='Click para aprovar o atestado'
-                  id='approv'
-                  className={`border border-solid border-blue-400 rounded-full p-2 hover:bg-slate-400 ${(item.aprove=='aprovado') ? 'bg-amber-200' : 'bg-white'}`}
-                  onClick={() => handleButtonClick(item.CPF, 'aprovado')}>
-                  <TbFileLike size={18} color={'#090'} />
-                </button>
-              </p>
             </div>
             <div className="flex items-center justify-center px-2 py-4 sm:flex">
               <p className="font-medium text-dark">
