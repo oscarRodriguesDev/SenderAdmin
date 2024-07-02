7cfd5343789605181932e458df7e44f127496be1
Oscar <oskharm12@gmail.com>
2024-07-01 21:12:24 -0300

    alteração no package.json

--- src/app/(auth)/auth/authEmail.ts
+++ src/app/(auth)/auth/authEmail.ts
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from "firebase/auth";
 import { getDatabase, ref, get,set,remove, } from 'firebase/database';
 import { redirect } from "next/navigation";
-import { deprecate } from "util";
+
 
 
 // Configurações do Firebase
@@ -14,13 +14,13 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inicialize o Firebase
-export const APP = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const authConfig = getAuth();
-const database = getDatabase(APP); 
+const database = getDatabase(app); 
 
 
 
